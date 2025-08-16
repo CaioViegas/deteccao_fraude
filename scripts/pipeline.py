3c92b39eba38151ea49693fef1095d76986ee030
CaioViegas <caio.costaviegas@gmail.com>
2025-08-15 22:31:53 -0300

    Finalizado o arquivo de pipeline

---
+++ scripts/pipeline.py
@@ -0,0 +1,22 @@
+import sys
+import pandas as pd
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parent))
+
+from src.etl.extract import download_dataset
+from src.etl.transform import DataTransformer
+from configs.paths import get_project_paths
+
+if __name__ == "__main__":
+    download_dataset(
+        "dnkumars/cybersecurity-intrusion-detection-dataset",
+        file_extension=".csv"
+    )
+
+    paths = get_project_paths()
+    raw_dir = paths['RAW']
+    df = pd.read_csv(raw_dir / "cybersecurity_intrusion_data.csv")
+
+    transformer = DataTransformer(df)
+    transformer.transform()
